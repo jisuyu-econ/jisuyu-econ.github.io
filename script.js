a10c777f0dce5f49fe363c24baabb76948df4775
jisuyu-econ <jisuyu@gmail.com>
2025-09-24 15:42:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-document.getElementById("year").textContent = new Date().getFullYear();
+document.querySelectorAll('.menu a').forEach(a => {
+if (a.href === window.location.href) a.setAttribute('aria-current','page');
+});
